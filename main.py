8e6c6c434c0c3b97368cbbd5ba3ae0642996b8ad
Erik <erik@dhcp114.merunorth110.iit.edu>
2019-10-17 13:48:15 -0500

    login page good, added main page

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def check_database(username, password):
+    # this function should check either a json
+    # doc or check an actual DB (more work)
+    return True
+
+
+@app.route('/', methods=["GET", "POST"])
 def login():
-    return render_template("login_page.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        success = check_database(username, password)
+        if not success:
+            return render_template("login.html")
+        else:
+            return render_template("main.html")
+
+
+@app.route("/home")
+def main():
+    return render_template("main.html")
